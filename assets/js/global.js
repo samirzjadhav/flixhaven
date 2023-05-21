1f8b728824241548fdcd0b3c4e81fed5cf955168
Samir Jadhav <samirzjadhav@gmail.com>
2023-05-21 13:52:50 +0530

    feat: add script for searchbox

---
+++ assets/js/global.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// ADD EVENT ON MULTIPLE ELEMENTS
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (const elem of elements) elem.addEventListener(eventType, callback);
+};
+
+// Toggle search box in mobile device || small screen
+
+const searchBox = document.querySelector("[search-box]");
+const searchTogglers = document.querySelectorAll("[search-toggler]");
+
+addEventOnElements(searchTogglers, "click", function () {
+  searchBox.classList.toggle("active");
+});
