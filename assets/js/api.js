25d647332e81f2777cd7fe104b99c3a3c542af17
Samir Jadhav <samirzjadhav@gmail.com>
2023-07-08 21:43:24 +0530

    feat: fetch data form api

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,3 +2,17 @@
 
 const api_key = "24fa3c2f5ede86d67022ddea455f8ffe";
 const imageBaseURL = "https://image.tmdb.org/t/p/";
+
+/**
+ * fetch data from a server using the "url" and presses
+ * the result in json data to a 'callback' function,
+ * along with an optional parameter if has 'optionalparam'.
+ */
+
+const fetchDataFromServer = function (url, callback, optionalparam) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => callback(data, optionalparam));
+};
+
+export { imageBaseURl, api_key, fetchDataFromServer };
