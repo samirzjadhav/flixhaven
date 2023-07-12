5a845a35221d54bb3be3205fe272a69e6710a1ce
Samir Jadhav <samirzjadhav@gmail.com>
2023-07-12 14:42:52 +0530

    feat: fetch movie information on web page

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,4 +35,76 @@ const heroBanner = function ({ results: movieList }) {
   const banner = document.createElement("section");
   banner.classList.add("banner");
   banner.ariaLabel = "Popular Movies";
+
+  banner.innerHTML = `
+    <div class="banner-slider"></div>
+    <div class="slider-control">
+      <div class="control-inner">
+        <button class="poster-box slider-item">
+          <img
+            src="./assets/images/slider-control.jpg"
+            alt="Slide to Puss in Boots: The Last Wish"
+            loading="lazy"
+            draggable="false"
+            class="img-cover"
+          />
+        </button>
+      </div>
+    </div>
+  `;
+
+  let controlItemIndex = 0;
+  for (const [index, movie] of movieList.entries()) {
+    const {
+      backdrop_path,
+      title,
+      release_date,
+      genre_ids,
+      overview,
+      poster_path,
+      vote_average,
+      id,
+      papularity,
+    } = movie;
+
+    const sliderItem = document.createElement("div");
+    sliderItem.classList.add("slider-item");
+    sliderItem.setAttribute("slider-item", "");
+
+    sliderItem.innerHTML = `
+      <img
+        src="${imageBaseURL}w1280${backdrop_path}"
+        alt="${title}"
+        class="img-cover"
+        loading="${index === 0 ? "eager" : "lazy"}"
+      />
+      <div class="banner-content">
+        <h2 class="heading">${title}</h2>
+        <div class="meta-list">
+          <div class="meta-item">${release_date.split("_")[0]}</div>
+          <div class="meta-item card-badge">${vote_average.toFixed(1)}</div>
+        </div>
+        <p class="genre">${genreList.asString(genre_ids)}</p>
+        <p class="banner-text">${overview}</p>
+        <a href="./detail.html" class="btn">
+          <img
+            src="./assets/images/play_circle.png"
+            alt="play circle"
+            height="24"
+            width="24"
+            aria-hidden="true"
+          />
+          <span class="span">Watch Now</span>
+        </a>
+      </div>
+    `;
+
+    banner.querySelector(".banner-slider").appendChild(sliderItem);
+
+    const controlItem = document.createElement("button");
+    controlItem.classList.add("poster-box", "slider-item");
+    controlItem.setAttribute("slider-control", `${controlItemIndex}`);
+
+    controlItemIndex++;
+  }
 };
