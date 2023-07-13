6c5297fd208562fe78e9016623b503bb50e8bf64
Samir Jadhav <samirzjadhav@gmail.com>
2023-07-13 13:07:16 +0530

    feat: add click event on slider-control to switch to new movie banner

--- assets/js/index.js
+++ assets/js/index.js
@@ -108,4 +108,33 @@ const heroBanner = function ({ results: movieList }) {
   }
 
   pageContent.appendChild(banner);
+
+  addHeroSlide();
+};
+
+// Hero slider functionality
+const addHeroSlide = function () {
+  const sliderItems = document.querySelectorAll("[slider-item]");
+  const sliderControls = document.querySelectorAll("[slider-control]");
+
+  let lastSliderItem = sliderItems[0];
+  let lastSliderControl = sliderControls[0];
+
+  lastSliderItem.classList.add("active");
+  lastSliderControl.classList.add("active");
+
+  const sliderStart = function () {
+    lastSliderItem.classList.remove("active");
+    lastSliderControl.classList.remove("active");
+
+    sliderItems[Number(this.getAttribute("slider-control"))].classList.add(
+      "active"
+    );
+    this.classList.add("active");
+
+    lastSliderItem = sliderItems[Number(this.getAttribute("slider-control"))];
+    lastSliderControl = this;
+  };
+
+  addEventOnElements(sliderControls, "click", sliderStart);
 };
