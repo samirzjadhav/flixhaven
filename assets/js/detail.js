e9e627aabd06fe9736eb9de6ef47d1725f13a4b7
Samir Jadhav <samirzjadhav@gmail.com>
2023-07-15 13:12:47 +0530

    feat: add id fetch script for cast directors= movieid

---
+++ assets/js/detail.js
@@ -0,0 +1,45 @@
+"use Script";
+
+import { api_key, imageBaseURL, fetchDataFromServer } from "./api.js";
+import { sidebar } from "./sidebar.js";
+import { createMovieCard } from "./movie-card.js";
+
+const movieId = window.localStorage.getItem("movieId");
+const pageContent = document.querySelector("[page-content]");
+
+sidebar();
+
+const getGenres = function (genreList) {
+  const newGenreList = [];
+
+  for (const { name } of genreList) newGenreList.push(name);
+  return newGenreList.join(", ");
+};
+
+const getCasts = function (castList) {
+  const newCastList = [];
+
+  for (let i = 0, len = castList.length; i < len && i < 10; i++) {
+    const { name } = castList[i];
+    newCastList.push(name);
+  }
+
+  return newCastList.join(", ");
+};
+
+const getDirectors = function (crewList) {
+  const directors = crewList.filter(({ job }) => job === "Director");
+
+  const directorList = [];
+  for (const { name } of directors) directorList.push(name);
+
+  return directorList.join(", ");
+};
+
+// Return all Trailers and Teasers as array
+const filterVideos = function (videoList) {
+  return videoList.filter(
+    ({ type, site }) =>
+      (type === "Trailer" || type === "Teaser") && site === "YouTube"
+  );
+};
