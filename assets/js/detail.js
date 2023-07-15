cf2e570ee9f373eb7f911067fea2107939527fe5
Samir Jadhav <samirzjadhav@gmail.com>
2023-07-15 13:14:02 +0530

    feat: add details about movie and fetch it from TMDB

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -43,3 +43,106 @@ const filterVideos = function (videoList) {
       (type === "Trailer" || type === "Teaser") && site === "YouTube"
   );
 };
+
+fetchDataFromServer(
+  `https://api.themoviedb.org/3/movie/${movieId}?api_key=${api_key}&append_to_response=casts,videos,images,releases`,
+  function (movie) {
+    const {
+      backdrop_path,
+      title,
+      release_date,
+      genre_ids,
+      overview,
+      poster_path,
+      vote_average,
+      releases: {
+        countries: [{ certification }],
+      },
+      genres,
+      casts: { cast, crew },
+      videos: { results: videos },
+      runtime,
+    } = movie;
+
+    document.title = `${title} - FlixHeaven`;
+    const movieDetail = document.createElement("div");
+    movieDetail.classList.add("movie-detail");
+
+    movieDetail.innerHTML = `
+      <div
+        class="backdrop-image"
+        style="background-image: url('${imageBaseURL}${"w1280" || "original"}${
+      backdrop_path || poster_path
+    }')">
+      </div>
+      <figure class="poster-box movie-poster">
+        <img
+          src="${imageBaseURL}w342${poster_path}"
+          alt="${title}"
+          class="img-cover"
+        />
+      </figure>
+      <div class="detail-box">
+        <div class="detail-content">
+          <h1 class="heading">${title}</h1>
+          <div class="meta-list">
+            <div class="meta-item">
+              <img
+                src="./assets/images/star.png"
+                alt="rating"
+                width="20"
+                height="20"
+              />
+              <span class="span">${vote_average.toFixed(1)}</span>
+            </div>
+            <div class="separator"></div>
+            <div class="meta-item">${runtime}m</div>
+            <div class="separator"></div>
+            <div class="meta-item">${release_date}</div>
+            <div class="meta-item card-badge">${certification}</div>
+          </div>
+          <p class="genre">${getGenres(genres)}</p>
+          <p class="overview"> ${overview}</p>
+          <ul class="detail-list">
+            <div class="list-item">
+              <p class="list-name">Starring</p>
+              <p>${getCasts(cast)}</p>
+            </div>
+            <div class="list-item">
+              <p class="list-name">Directed By</p>
+              <p>${getDirectors(crew)}</p>
+            </div>
+          </ul>
+        </div>
+        <div class="title-wrapper">
+          <h3 class="title-large">Trailers and Clips</h3>
+        </div>
+        <div class="slider-list">
+          <div class="slider-inner"></div>
+        </div>
+      </div>
+    `;
+
+    for (const { key, name } of filterVideos(videos)) {
+      const videoCard = document.createElement("div");
+      videoCard.classList.add("video-card");
+
+      videoCard.innerHTML = `
+        <iframe
+          width="500"
+          height="294"
+          src="https://www.youtube.com/embed/${key}?&theme=dark&color=white&rel=0"
+          frameborder="0"
+          allowfullscreen="1"
+          title="${name}"
+          class="img-cover"
+          loading="lazy"
+        ></iframe>
+      `;
+
+      movieDetail.querySelector(".slider-inner").appendChild(videoCard);
+    }
+
+    pageContent.appendChild(movieDetail);
+  }
+);
